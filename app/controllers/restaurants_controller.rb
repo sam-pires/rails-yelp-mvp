2f3b278c065e019b44c679d337cb215e333f79bc
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-01 12:24:39 +0100

    added restaurant controller and seeds the db"

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,16 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0492893837',
+    category:     'belgian'
+  },
+  {
+    name:         'Sushi Fast',
+    address:      '8 Rue Creuson, 34900 Chambery',
+    phone_number: '0592433837',
+    category:     'japanese'
+  },
+  {
+    name:         'La bouillabaisse',
+    address:      'Avenue du vieux port, 13000 Marseille',
+    phone_number: '0334893837',
+    category:     'italian'
+  },
+  {
+    name:         'Raviolo',
+    address:      '3 Canal saint martin, 75013 Paris',
+    phone_number: '0897453232',
+    category:     'chinese'
+  },
+  {
+    name:         'La belle marmitte',
+    address:      '77 rue de calvados, 28799 Bréchet',
+    phone_number: '0492898080',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
